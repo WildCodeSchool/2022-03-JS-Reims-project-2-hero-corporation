deb13564ec0f0ae75b876e23b9e646b3be5cbc42
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-04-14 17:11:41 +0200

    call api in Road.jsx

--- frontend/src/pages/Road.jsx
+++ frontend/src/pages/Road.jsx
@@ -1,14 +1,30 @@
+import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
+import axios from "axios";
+
 import Home from "./Home";
 import SelectHero from "./SelectHero";
 import Fight from "./Fight";
 
 function Road() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://akabab.github.io/superhero-api/api/all.json")
+      .then((response) => {
+        setCharacters(response.data);
+      });
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/selecthero" element={<SelectHero />} />
-      <Route path="/fight" element={<Fight />} />
+      <Route
+        path="/selecthero"
+        element={<SelectHero characters={characters} />}
+      />
+      <Route path="/fight" element={<Fight characters={characters} />} />
     </Routes>
   );
 }
