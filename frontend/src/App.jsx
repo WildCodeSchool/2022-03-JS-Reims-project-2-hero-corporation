d7a89a3a36d7376a7411c2cd85a4033b63987580
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-04-14 15:04:31 +0200

    Add import for App.css in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
 import Footer from "./components/footer/Footer";
+import "./App.css";
 
 function App() {
   return (
