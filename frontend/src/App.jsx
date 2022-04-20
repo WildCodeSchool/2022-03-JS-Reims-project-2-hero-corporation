5c1d6d74a2eab19357baa76a44e239a0758015ac
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-04-20 16:27:04 +0200

    Pass props from App.jsx to Main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      <Main selectedHero={selectedHero} setSelectedHero={setSelectedHero} />
       <Footer />
     </div>
   );
