72981bd919c3bc358d948e955e79df23ca8ada8b
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-04-20 16:48:43 +0200

    Pass props from Main.jsx to Road.jsx

--- frontend/src/components/main/Main.jsx
+++ frontend/src/components/main/Main.jsx
@@ -1,12 +1,20 @@
+import PropTypes from "prop-types";
 import Road from "../../pages/Road";
+import Character from "../Character";
 
-function Main() {
+function Main(props) {
+  const { selectedHero, setSelectedHero } = props;
   return (
     <>
       <p>Main</p>
-      <Road />
+      <Road selectedHero={selectedHero} setSelectedHero={setSelectedHero} />
     </>
   );
 }
+
+Main.propTypes = {
+  selectedHero: Character.propTypes.character.isRequired,
+  setSelectedHero: PropTypes.func.isRequired,
+};
 
 export default Main;
