9317d3b6406be150a38bb5d7ff08defa7badd7f7
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-04-20 16:25:06 +0200

    Add selectedHero state in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
 import Footer from "./components/footer/Footer";
 import "./App.css";
 
 function App() {
+  const [selectedHero, setSelectedHero] = useState(null);
   return (
     <div className="App">
       <Header />
