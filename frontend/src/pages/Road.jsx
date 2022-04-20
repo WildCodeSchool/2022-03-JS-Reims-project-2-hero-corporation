c802743351d3336dc2d34634f5f958968741fdf2
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-04-20 17:15:52 +0200

    Pass props from Road.jsx to SelectHero.jsx &Fight.jsx

--- frontend/src/pages/Road.jsx
+++ frontend/src/pages/Road.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import axios from "axios";
@@ -5,9 +6,11 @@ import axios from "axios";
 import Home from "./Home";
 import SelectHero from "./SelectHero";
 import Fight from "./Fight";
+import CharacterType from "../components/CharacterType";
 
-function Road() {
+function Road(props) {
   const [characters, setCharacters] = useState([]);
+  const { selectedHero, setSelectedHero } = props;
 
   useEffect(() => {
     axios
@@ -22,11 +25,35 @@ function Road() {
       <Route path="/" element={<Home />} />
       <Route
         path="/selecthero"
-        element={<SelectHero characters={characters} />}
+        element={
+          <SelectHero
+            characters={characters}
+            selectedHero={selectedHero}
+            setSelectedHero={setSelectedHero}
+          />
+        }
       />
-      <Route path="/fight" element={<Fight characters={characters} />} />
+      <Route
+        path="/fight"
+        element={
+          <Fight
+            characters={characters}
+            selectedHero={selectedHero}
+            setSelectedHero={setSelectedHero}
+          />
+        }
+      />
     </Routes>
   );
 }
+
+Road.propTypes = {
+  selectedHero: CharacterType,
+  setSelectedHero: PropTypes.func.isRequired,
+};
+
+Road.defaultProps = {
+  selectedHero: undefined,
+};
 
 export default Road;
