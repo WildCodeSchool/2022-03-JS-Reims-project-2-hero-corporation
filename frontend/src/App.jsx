0288ff345d8027ecf3352e794544488dfeda7d75
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-04-25 09:10:54 +0200

    delete props selected hero on App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from "react";
 import Header from "./components/header/Header";
 import Main from "./components/main/Main";
 import Footer from "./components/footer/Footer";
 import "./App.css";
 
 function App() {
-  const [selectedHero, setSelectedHero] = useState();
   return (
     <div className="App">
       <Header />
-      <Main selectedHero={selectedHero} setSelectedHero={setSelectedHero} />
+      <Main />
       <Footer />
     </div>
   );
