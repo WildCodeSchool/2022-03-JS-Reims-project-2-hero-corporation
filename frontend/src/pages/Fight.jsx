64376742e1d6c20e5ee47f5719fc7583e2a6d253
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-04-25 09:58:59 +0200

    delete .filter.map on characters and use character with props boss

--- frontend/src/pages/Fight.jsx
+++ frontend/src/pages/Fight.jsx
@@ -1,9 +1,52 @@
 import { useEffect, useState } from "react";
 import Character from "../components/Character";
 
-function Fight({ characters }) {
-  const heroesOnly = (element) => element.id === 346;
-  const boss = (element) => element.id === 655;
+function Fight({ boss, hero }) {
+  const [bossLife, setBossLife] = useState(0);
+  const [bossWeakness, setBossWeakness] = useState();
+
+  useEffect(() => {
+    setBossLife(
+      boss.powerstats.intelligence +
+        boss.powerstats.strength +
+        boss.powerstats.speed +
+        boss.powerstats.power
+    );
+
+    const weaknessValue = Math.min(
+      boss.powerstats.intelligence,
+      boss.powerstats.strength,
+      boss.powerstats.speed,
+      boss.powerstats.power
+    );
+
+    if (weaknessValue === boss.powerstats.intelligence) {
+      setBossWeakness("intelligence");
+    }
+    if (weaknessValue === boss.powerstats.strength) {
+      setBossWeakness("strength");
+    }
+    if (weaknessValue === boss.powerstats.speed) {
+      setBossWeakness("speed");
+    }
+    if (weaknessValue === boss.powerstats.power) {
+      setBossWeakness("power");
+    }
+  }, [boss]);
+
+  const useWeapon = (weapon) => {
+    if (weapon === bossWeakness) {
+      setBossLife(Math.max(bossLife - 10, 0));
+    } else {
+      setBossLife(Math.max(bossLife - 1, 0));
+    }
+  };
+
+  useEffect(() => {
+    if (bossLife === 0) {
+      console.warn("Hello");
+    }
+  }, [bossLife]);
 
   return (
     <>
