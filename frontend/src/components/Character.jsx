0cc01123edfb39a61ba759e58eedb49a91fd5105
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-04-27 11:08:30 +0200

    use props details for not show stats in fight with Character.jsx

--- frontend/src/components/Character.jsx
+++ frontend/src/components/Character.jsx
@@ -1,38 +1,43 @@
+import PropTypes from "prop-types";
 import "./Character.css";
 import CharacterType from "./CharacterType";
 
-function Character({ character }) {
+function Character({ character, details }) {
   return (
     <figure>
       <img src={character.images.sm} alt={character.name} />
       <figcaption>
         <h3>{character.name}</h3>
-        <p>
-          Intelligence :
-          <progress
-            id="intelligence"
-            max="100"
-            value={character.powerstats.intelligence}
-          >
-            {character.powerstats.intelligence}
-          </progress>
-          Strength :
-          <progress
-            id="strength"
-            max="100"
-            value={character.powerstats.strength}
-          >
-            {character.powerstats.strength}
-          </progress>
-          Speed :
-          <progress id="speed" max="100" value={character.powerstats.speed}>
-            {character.powerstats.speed}
-          </progress>
-          Power :
-          <progress id="power" max="100" value={character.powerstats.power}>
-            {character.powerstats.power}
-          </progress>
-        </p>
+        {details && (
+          <p>
+            Intelligence :
+            <progress
+              id="intelligence"
+              max="100"
+              value={character.powerstats.intelligence}
+              // state heroIntelligence => set change en function des regle du jeu, default egale character.powerstats.intelligence
+              // value={heroIntelligence}
+            >
+              {character.powerstats.intelligence}
+            </progress>
+            Strength :
+            <progress
+              id="strength"
+              max="100"
+              value={character.powerstats.strength}
+            >
+              {character.powerstats.strength}
+            </progress>
+            Speed :
+            <progress id="speed" max="100" value={character.powerstats.speed}>
+              {character.powerstats.speed}
+            </progress>
+            Power :
+            <progress id="power" max="100" value={character.powerstats.power}>
+              {character.powerstats.power}
+            </progress>
+          </p>
+        )}
       </figcaption>
     </figure>
   );
@@ -40,6 +45,9 @@ function Character({ character }) {
 
 Character.propTypes = {
   character: CharacterType.isRequired,
+  details: PropTypes.bool,
 };
-
+Character.defaultProps = {
+  details: false,
+};
 export default Character;
