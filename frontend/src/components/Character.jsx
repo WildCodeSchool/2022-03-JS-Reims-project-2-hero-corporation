f05027a4f3b8d01d0afe807ae4f48d82778f293a
ALEXANDRA DE_BRITO <alexandradebrito@live.fr>
2022-05-02 16:47:38 +0200

    update Character.jsx with new classNames

--- frontend/src/components/Character.jsx
+++ frontend/src/components/Character.jsx
@@ -4,62 +4,53 @@ import CharacterType from "./CharacterType";
 
 function Character({ character, details }) {
   return (
-    <figure className="selectHero fight">
-      <figcaption className="figure-container">
-        <div className="figure-background">
-          <div className="hero-image">
-            <img src={character.images.sm} alt={character.name} />
+    <article className="hero-container">
+      <h3 className="hero-name">{character.name}</h3>
+      <figure className="hero-image-container">
+        <img
+          className="hero-image"
+          src={character.images.sm}
+          alt={character.name}
+        />
+        <figcaption className="hero-biography">Lorem ipsum</figcaption>
+      </figure>
+      {details && (
+        <div className="hero-stats">
+          <div className="intelligence-stats">
+            Intelligence :
+            <progress
+              id="intelligence"
+              max="100"
+              value={character.powerstats.intelligence}
+            >
+              {character.powerstats.intelligence}
+            </progress>
           </div>
-          <h3 className="hero-name">{character.name}</h3>
-          <div className="all-stats">
-            {details && (
-              <p>
-                <div className="intelligence-stats">
-                  Intelligence :
-                  <progress
-                    id="intelligence"
-                    max="100"
-                    value={character.powerstats.intelligence}
-                  >
-                    {character.powerstats.intelligence}
-                  </progress>
-                </div>
-                <div className="strength-stats">
-                  Strength :
-                  <progress
-                    id="strength"
-                    max="100"
-                    value={character.powerstats.strength}
-                  >
-                    {character.powerstats.strength}
-                  </progress>
-                </div>
-                <div className="speed-stats">
-                  Speed :
-                  <progress
-                    id="speed"
-                    max="100"
-                    value={character.powerstats.speed}
-                  >
-                    {character.powerstats.speed}
-                  </progress>
-                </div>
-                <div className="power-stats">
-                  Power :
-                  <progress
-                    id="power"
-                    max="100"
-                    value={character.powerstats.power}
-                  >
-                    {character.powerstats.power}
-                  </progress>
-                </div>
-              </p>
-            )}
+          <div className="strength-stats">
+            Strength :
+            <progress
+              id="strength"
+              max="100"
+              value={character.powerstats.strength}
+            >
+              {character.powerstats.strength}
+            </progress>
+          </div>
+          <div className="speed-stats">
+            Speed :
+            <progress id="speed" max="100" value={character.powerstats.speed}>
+              {character.powerstats.speed}
+            </progress>
+          </div>
+          <div className="power-stats">
+            Power :
+            <progress id="power" max="100" value={character.powerstats.power}>
+              {character.powerstats.power}
+            </progress>
           </div>
         </div>
-      </figcaption>
-    </figure>
+      )}
+    </article>
   );
 }
 
