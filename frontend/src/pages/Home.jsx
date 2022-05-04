2b6334228a076b2c28afe1104a8716352133ab17
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-05-04 10:15:17 +0200

    add link to select hero in home and resize logo

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,22 @@
 import Logo from "../assets/images/hero-corporation-logo-mobile.png";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <img
-      src={Logo}
-      alt="hero-corporation-logo"
-      className="hero-corporation-logo"
-    />
+    <>
+      <img
+        src={Logo}
+        alt="hero-corporation-logo"
+        className="hero-corporation-logo"
+      />
+      <Link to="/selecthero">
+        <img
+          src="/src/assets/images/start.png"
+          alt="start img"
+          className="start"
+        />
+      </Link>
+    </>
   );
 }
 
