ca95ea4f97f65ce68a46f010fdc5da6af21012c9
ALEXANDRA DE_BRITO <alexandradebrito@live.fr>
2022-05-10 14:18:03 +0200

    Rebase

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,22 +1,33 @@
 import { Link } from "react-router-dom";
 import Logo from "../assets/images/hero-corporation-logo-mobile.png";
+import "./Home.css";
 
 function Home() {
   return (
-    <div className="flex-column">
-      <img
-        src={Logo}
-        alt="hero-corporation-logo"
-        className="hero-corporation-logo"
-      />
-      <Link to="/selecthero">
+    <>
+      <div className="banner">
+        <div className="banner-presentation">Ready for a new adventure ?</div>
+        <div className="edition-number-container">
+          <div className="edition-number">
+            <h2>#01</h2>May 2022
+          </div>
+        </div>
+      </div>
+      <div className="flex-column">
         <img
-          src="/src/assets/images/HeroCorporation-end-elements-23.png"
-          alt="play img"
-          className="play"
+          src={Logo}
+          alt="hero-corporation-logo"
+          className="hero-corporation-logo"
         />
-      </Link>
-    </div>
+        <Link to="/selecthero">
+          <img
+            src="/src/assets/images/HeroCorporation-end-elements-23.png"
+            alt="play img"
+            className="play"
+          />
+        </Link>
+      </div>
+    </>
   );
 }
 
