d6fd3173944791352ecff0e027941608b6212e6b
ALEXANDRA DE_BRITO <alexandradebrito@live.fr>
2022-05-11 09:40:09 +0200

    Banner for the Endgame done

--- frontend/src/pages/Endgame.jsx
+++ frontend/src/pages/Endgame.jsx
@@ -1,16 +1,26 @@
+import "./Endgame.css";
+
 function Endgame() {
   return (
-    <div className="endgame">
-      <img
-        src="src/assets/images/HeroCorporation-end-elements-24.png"
-        alt="Congratulations !"
-        className="congratulation"
-      />
-      <img
-        src="src/assets/images/play-again-element.png"
-        alt="Play Again"
-        className="playagain"
-      />
+    <div className="main-endgame">
+      <div className="bande-header">
+        <h2 className="bande-win">Mission accomplished !</h2>
+        <img
+          src="src/assets/images/star-element.png"
+          alt="star"
+          className="star-image1"
+        />
+        <img
+          src="src/assets/images/star-element.png"
+          alt="star"
+          className="star-image2"
+        />
+        <img
+          src="src/assets/images/star-element.png"
+          alt="star"
+          className="star-image3"
+        />
+      </div>
     </div>
   );
 }
