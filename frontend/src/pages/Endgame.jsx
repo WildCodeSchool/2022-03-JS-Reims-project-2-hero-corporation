273106c00d16dbd206400c85d91f814f7b21d38c
ALEXANDRA DE_BRITO <alexandradebrito@live.fr>
2022-05-11 09:52:00 +0200

    Bublle speech and Play again done

--- frontend/src/pages/Endgame.jsx
+++ frontend/src/pages/Endgame.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./Endgame.css";
 
 function Endgame() {
@@ -5,6 +6,7 @@ function Endgame() {
     <div className="main-endgame">
       <div className="bande-header">
         <h2 className="bande-win">Mission accomplished !</h2>
+
         <img
           src="src/assets/images/star-element.png"
           alt="star"
@@ -21,6 +23,30 @@ function Endgame() {
           className="star-image3"
         />
       </div>
+      <img
+        src="/src/assets/images/congratulations-element.png"
+        alt="Congratulations !"
+        className="congratulations-element"
+      />
+
+      <div className="comic-bubble-container">
+        <img
+          src="/src/assets/images/comic-bubble.png"
+          className="comic-bubble"
+          alt="comic bubble"
+        />
+        <p className="bubble-speech">
+          You have stopped Thanos from destroying the planet ! And so once
+          again, the day is saved thanks to Hero Corporation !
+        </p>
+      </div>
+      <Link to="/selecthero">
+        <img
+          src="/src/assets/images/play-again-element.png"
+          alt="play again"
+          className="play-again"
+        />
+      </Link>
     </div>
   );
 }
