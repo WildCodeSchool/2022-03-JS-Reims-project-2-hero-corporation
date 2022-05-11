dc50687cda2ab63c4d581f25ebfac7302f52a4ca
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-05-11 09:56:26 +0200

    Fix rules typo

--- frontend/src/components/RulesModal.jsx
+++ frontend/src/components/RulesModal.jsx
@@ -6,19 +6,20 @@ function RulesModal() {
       <div className="boxrules box2rules flex justify-center items-center flex-col">
         <h2>Règles</h2>
         <ul>
-          <li>Une fois sur l&apos;écran de sélection du héro</li>
-          <li>Fait défiller les héro avec les flêches</li>
-          <li>Une fois ton choix fait appuie sur start</li>
+          <li>Une fois sur l&apos;écran de sélection du héros</li>
+          <li>Fais défiler les héros avec les flêches</li>
+          <li>Une fois ton héros séléctionné appuie sur start</li>
+          <li>Le combat commence</li>
           <li>
-            Le combat commence choisi parmis tes 4 attaques elles correspondes à
-            tes capacitées.
+            Tu as la possibilité de choisir entre 4 attaques différentes qui
+            représentent ton élément de frappe
           </li>
-          <li>Certaine attaques infligent plus de dégat que d&apos;autres</li>
-          <li>Attention plus une stat diminue moins les dégats sont élevés</li>
+          <li>Certaines attaques infligent plus de dégats que d&apos;autres</li>
           <li>
-            Réfléchis bien à l&apos;utilisation de tes points d&apos;attaques
-            Pense au temps il n&apos;est pas illimité
+            Attention: plus une stat diminue, moins ses dégats seront élevés
           </li>
+          <li>Réfléchis bien à l&apos;utilisation de tes compétences</li>
+          <li>Ne traine pas, il ne te reste que 5 minutes !</li>
         </ul>
       </div>
     </div>
